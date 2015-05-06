e91f1391c9c450cd636d341cc23a0168100c1b9d
Victor <vasilica.victor@gmail.com>
2015-05-06 16:57:26 +0300

    adding authentication support

---
+++ MetalMind/Authentication.swift
@@ -0,0 +1,10 @@
+//
+//  Authentication.swift
+//  MetalMind
+//
+//  Created by Victor Vasilica on 5/6/15.
+//  Copyright (c) 2015 simple. All rights reserved.
+//
+
+import Foundation
+import Security
